f061d2f63869ebd18f38856e43175d7c420fa4b3
Marco <marcopa84@hotmail.it>
2020-03-16 16:44:35 +0100

    SCSS OK!

--- resources/views/layout/StaticPageLayout.blade.php
+++ resources/views/layout/StaticPageLayout.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <title>Php Dischi</title>
-</head>
-
-<body>
+@include('layout.partials._header'));
     <div class="container">
         <header>
             <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
